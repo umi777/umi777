dc3c7ecd6168c4725c380be5f5e4722e3d1eec67
Максим Иванович <umi-9956@yandex.ru>
2022-08-20 21:25:20 +0700

    update test_cart.php

--- test_cart.php
+++ test_cart.php
@@ -2,40 +2,67 @@
 // https://estrin.pw/bitrix-d7-snippets/s/sale-basket/
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $basket = Bitrix\Sale\Basket::loadItemsForFUser(Bitrix\Sale\Fuser::getId(), Bitrix\Main\Context::getCurrent()->getSite());
-$products = array(
-    array('PRODUCT_ID' => 3376, 'PRICE' => 400, 'BASE_PRICE' => 400, 'CURRENCY' => 'RUB', 'QUANTITY' => 1),
-    array(
-      'PRODUCT_ID' => 3394, 
-        'QUANTITY' => 2,
-        'CURRENCY' => \Bitrix\Currency\CurrencyManager::getBaseCurrency(),
-        'LID' => Bitrix\Main\Context::getCurrent()->getSite(),
-        'PRODUCT_PROVIDER_CLASS' => 'CCatalogProductProvider')
-            );
-$product = \Bitrix\Catalog\ProductTable::getList(['filter'=>['ID'=>3394]]);
-//print_r($product->fetch());
 $property =     array(
     'NAME' => 'Соус',
     'CODE' => 'SAUSE',
     'VALUE' => 'Обычный',
     'SORT' => 100,
 );
+$products = array(
+    array('PRODUCT_ID' => 3376, 'QUANTITY' => 1, 'PROPS'=>[$property]
+    ),
+    array('PRODUCT_ID' => 3394, 'QUANTITY' => 1, 'PROPS'=>[$property]
+    ),
+            );
+// $product = \Bitrix\Catalog\ProductTable::getList(['filter'=>['ID'=>3394]]);
+//print_r($product->fetch());
 //$basket = Bitrix\Sale\Basket::create(SITE_ID);
 echo '<pre>';
 foreach ($products as $product)
     {
-        $item = $basket->createItem("catalog", $product["PRODUCT_ID"]);
-        print_r($item);
-        $basket->save();
-        $basket->refresh();
-        unset($product["PRODUCT_ID"]);
-        $item->save();
-        $item->setFields($product);
-        print_r($item);
-        $basketPropertyCollection = $item->getPropertyCollection(); 
-        $basketPropertyCollection->setProperty([$property]);
-        $basketPropertyCollection->save();
-        $item->save();
-        //print_r($item->getPropertyCollection()->getPropertyValues());
+        $basketResult = \Bitrix\Catalog\Product\Basket::addProduct($product/*, $rewriteFields, $options */);
+if ($basketResult->isSuccess())
+{
+    $ressult['success'] = true;
+
+    $data = $basketResult->getData();
+
+    /* 
+
+    Array ( 
+
+        [ID] => 3 // basket id
+
+    )
+
+    */
+
+    $basket = \Bitrix\Sale\Basket::loadItemsForFUser(
+
+        \Bitrix\Sale\Fuser::getId(), 
+
+        \Bitrix\Main\Context::getCurrent()->getSite()
+
+    );
+
+    $refreshStrategy = \Bitrix\Sale\Basket\RefreshFactory::create(\Bitrix\Sale\Basket\RefreshFactory::TYPE_FULL);
+
+    $basket->refresh($refreshStrategy);    
+
+    $basket->save();
+
+}
+
+else
+
+{
+
+    $ressult['success'] = false;
+
+    $ressult['error'] = $basketResult->getErrorMessages();
+
+}
+
         
     }
 
